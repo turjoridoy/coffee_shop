717e67ea28352fac13a00c88a8e0ca5001009390
Sadman Hossain Ridoy <turjoridoy@gmail.com>
2025-07-29 00:59:27 +0600

    fix deployed code

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate
