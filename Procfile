04b0d9703bdd4973d49382afbc1b41b0d3297533
Sadman Hossain Ridoy <turjoridoy@gmail.com>
2025-07-29 17:55:42 +0600

    db change

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn conf.wsgi:application
--- conf/settings.py
+++ conf/settings.py
@@ -92,8 +92,16 @@ WSGI_APPLICATION = "conf.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "u183730229_coffee_shop",
+        "USER": "u183730229_root",
+        "PASSWORD": "NtZYc@cuCz6@",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "OPTIONS": {
+            "charset": "utf8mb4",
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
