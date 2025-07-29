f504274563aecdf1f85ee722c302d391fa3003c1
Sadman Hossain Ridoy <turjoridoy@gmail.com>
2025-07-29 19:02:32 +0600

    db issue

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -15,4 +15,24 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "conf.settings"
 # Import Django WSGI application
 from django.core.wsgi import get_wsgi_application
 
+# Create the WSGI application
 application = get_wsgi_application()
+
+
+# For debugging - remove this in production
+def debug_application(environ, start_response):
+    """Debug application to test if Python is working"""
+    status = "200 OK"
+    output = b"Python is working! Django should be available."
+
+    response_headers = [
+        ("Content-type", "text/plain"),
+        ("Content-Length", str(len(output))),
+    ]
+    start_response(status, response_headers)
+
+    return [output]
+
+
+# Use debug application for testing
+application = debug_application
---
+++ test_python.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+print("Content-Type: text/plain")
+print()
+print("Python is working on this server!")
+print("If you can see this, Python is properly configured.")
